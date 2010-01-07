008fd40f73d9a2970e612946d17a57e888f9e3af
fdesan <fdesan@663970ea-d6ad-11de-829d-073b792d0d12>
2010-01-07 17:13:51 +0000

    code at least compiles

--- trunk/code/2apl_platform/environments/buildenv.sh
+++ trunk/code/2apl_platform/environments/buildenv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-jar cvfm cardtable.jar cardtable/Manifest.txt -C cardtable/ .
+javac -classpath "../apapl.jar:." cardtable/*java && jar cvfm cardtable.jar cardtable/Manifest.txt -C cardtable/ .
